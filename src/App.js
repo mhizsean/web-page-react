0fe49262ad4e6588ffa7b8932abfec0db49adada
mhizsean <adeyiesther01@gmail.com>
2020-02-16 22:45:26 +0100

    worked on the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <h2>A whole new world</h2>
+    <Navbar />
   );
 }
 
