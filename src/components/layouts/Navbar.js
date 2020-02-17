ac3233f9f0b04a348d90a2df66d7aa238dc5ba71
mhizsean <adeyiesther01@gmail.com>
2020-02-17 19:40:41 +0100

    fixed the toggle bar

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -10,7 +10,9 @@ function Navbar() {
                 <img src={logo} alt="" style={{width: '50px'}}/>    
             </Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <span>
+                    <i className="fas fa-bars" style={{color: '#fff'}}></i>
+                </span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
